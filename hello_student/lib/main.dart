ea0e5c50fcf3259033d03e2de66197e4a8710df3
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-27 02:28:08 +0300

    completed task 2

---
+++ hello_student/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Text(
+        'Привет! Меня зовут Наташа.\nЯ студентка группы ИСП-232.',
+      ),
+    ),
+  );
+}
