73ef5739d8ed7c33ccd52e6a9a5eca9f08468446
natalia <ponomarevanatasha16072007@gmail.com>
2026-05-27 02:53:53 +0300

    completed task 3 and completed labs

--- hello_student/lib/main.dart
+++ hello_student/lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Text(
-        'Привет! Меня зовут Наташа.\nЯ студентка группы ИСП-232.',
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Привет! Меня зовут Наташа.\nЯ студентка группы ИСП-232.'),
+          SizedBox(height: 20),
+          Image.asset('imgg/spanchbob.jpg', width: 250),
+        ],
       ),
     ),
   );
